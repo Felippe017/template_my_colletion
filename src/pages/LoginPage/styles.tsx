56bfcaa42432d6eed06e732f9583623ae11dd5e6
Felippe017 <felippecorrea2010@gmail.com>
2023-09-09 10:46:32 -0300

    feat/cria-pagina-login: adiciona reference para configuração de teste e um arquivo styles para a pasta loginPage

---
+++ src/pages/LoginPage/styles.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const LoginContainer = styled.body`
+   background-color: #2B3C51;
+   min-width: 100vw;
+`
+
+const ElementsContainer = styled.div`
+   /* width: 191px;
+   height: 40px;
+   top: 420px;
+   left: 769px; */
+
+`
+
+const Title = styled.h1`
+    font-weight: 400;
+    font-size: 30px;
+    font-family: Inter;
+`
+
+export {
+    LoginContainer,
+    Title,
+    ElementsContainer
+}
